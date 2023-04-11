be78d43d4fbcdf521b94962e213fa7ac801f1743
mary nyambura <115901653+marynk0@users.noreply.github.com>
2023-04-11 21:53:41 +0300

    Add files via upload

---
+++ percentage_people_vaccinated.sql
@@ -0,0 +1,9 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[total_new_vaccinations]
+      ,[percentage_people_vaccinated]
+  FROM [master].[dbo].[percentage_people_vaccinated]
+ 
